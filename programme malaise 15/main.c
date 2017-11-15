bb57f2bd0efd8cf087263e16588d62bf3e309ce7
Vieuxnorris <vieuxnorris@gmail.com>
2017-11-15 14:41:35 +0100

    Add files via upload

---
+++ programme malaise 15/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ligne 3
+#define colonne 3
+
+int main()
+{
+    int tab[ligne][colonne];
+    int i=0,j=0,reponse=0,compteur=0;
+
+    for(i=0;i<ligne;i++)
+    {
+        for(j=0;j<colonne;j++)
+        {
+            do
+            {
+                printf("entrez un nombre positif : ");
+                scanf("%d", &tab[i][j]);
+            }while(tab[i][j]<=0);
+            if (tab[i][j]%2==0)
+            {
+                compteur++;
+            }
+            reponse = reponse + tab[i][j];
+        }
+        printf("nombre de paire : %d \nsomme de la colonne : %d \n", compteur, reponse);
+    }
+}
