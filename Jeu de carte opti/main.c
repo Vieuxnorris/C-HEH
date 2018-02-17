2945d46267821c8953c4a13218d9010db91b23e5
Despair666 <36450845+Despair666@users.noreply.github.com>
2018-02-17 18:45:39 +0100

    Update main.c

--- Jeu de carte opti/main.c
+++ Jeu de carte opti/main.c
@@ -29,7 +29,7 @@ int main()
     int joueur_actuelle=0;
     int deck[13][4];
     int deck_couleur[13][4];
-    int fake_win[13] = {4,4,4,4,4,4,4,4,4,4,4,4,4,4}; // variable pour savoir le nombre max de carte chaque joueur
+    int fake_win[13] = {4,4,4,4,4,4,4,4,4,4,4,4,4,4}; // Variable pour connaître le nombre max de cartes de chaque joueur
     int nombre_de_carte = 52;
     int n;
 
@@ -57,7 +57,7 @@ int main()
         purge();
     }while(nombre_de_manche < 0 || nombre_de_manche > 999);
 
-    melange(deck,deck_couleur,joueur); // melange le deck
+    melange(deck,deck_couleur,joueur); // Mélange de deck
 
     do
     {
@@ -97,19 +97,19 @@ void melange(int deck[13][4],int deck_couleur[13][4],int joueur)
     int carte = 9;
     int random_carte = 3;
 
-    for(i=3;i<joueur;i++) // pour configurer les valeurs random des cartes des joueurs + celle du deck
+    for(i=3;i<joueur;i++) // Pour configurer les valeurs random des cartes des joueurs + celles du deck
     {
         carte--;
         random_carte++;
     }
 
-    for(i=0;i<carte;i++) //melange aléatoire pour le reste des cartes
+    for(i=0;i<carte;i++) //Mélange aléatoire pour le reste des cartes
     {
         for(j=0;j<4;j++)
         {
             do
             {
-                valeur_aleatoire_a = rand() %(13-random_carte)+random_carte; // random entre la valeur random et 13
+                valeur_aleatoire_a = rand() %(13-random_carte)+random_carte; // Random entre la valeur random et 13
                 valeur_aleatoire_b = rand() % 4;
             }while(Sans_Double[valeur_aleatoire_a][valeur_aleatoire_b] != 0);
             Sans_Double[valeur_aleatoire_a][valeur_aleatoire_b] = 1;
@@ -117,15 +117,15 @@ void melange(int deck[13][4],int deck_couleur[13][4],int joueur)
             deck_couleur[i][j] = valeur_aleatoire_b;
         }
     }
-    for(i=12;i>carte;i--) //melange aléatoire pour les joueurs
+    for(i=12;i>carte;i--) //Mélange aléatoire pour les joueurs
     {
         for(j=0;j<4;j++)
         {
             do
             {
-                valeur_aleatoire_a = rand() % random_carte; // random entre 0 et random
+                valeur_aleatoire_a = rand() % random_carte; // Random entre 0 et random
                 valeur_aleatoire_b = rand() % 4;
-            }while(Sans_Double[valeur_aleatoire_a][valeur_aleatoire_b] != 0); // verifie si il a pas déjà une valeur avec les mêmes valeurs
+            }while(Sans_Double[valeur_aleatoire_a][valeur_aleatoire_b] != 0); // Vérifie s'il n'y a pas déjà une valeur avec les mêmes valeurs
             Sans_Double[valeur_aleatoire_a][valeur_aleatoire_b] = 1;
             deck[i][j] = valeur_aleatoire_a;
             deck_couleur[i][j] = valeur_aleatoire_b;
@@ -153,8 +153,8 @@ void affichage(int deck[13][4],int deck_couleur[13][4], int joueur,int joueur_ac
 
     for(i=joueur_actuelle;i>=joueur_actuelle;i--)
     {
-        for(j=0;j<fake_win[i];j++) // scan de la valeur 0 à la valeur 4,5,6,7
-        {                          // tout depend de ce que la valeur du joueur [i] a comme nombre de carte max
+        for(j=0;j<fake_win[i];j++) // Scan de la valeur 0 aux valeurs 4,5,6,7
+        {                          // Tout depend de ce que la valeur du joueur [i] a comme nombre de carte max
             if(j > 3)
             {
                 deck[i][j] = deck[i-joueur][-1+temp];
@@ -164,8 +164,8 @@ void affichage(int deck[13][4],int deck_couleur[13][4], int joueur,int joueur_ac
             {
                 fake_win[i] = 4; //remet a 4 les cartes du joueur [i]
             }
-            printf("carte : %s \t couleur : %s \n", tableaux_graphique_carte[deck[i][j]],tableaux_graphique_couleur[deck_couleur[i][j]]);
-            if(deck[i][j] == deck[i][0]) //condition de victoire pour les cartes
+            printf("Carte : %s \t Couleur : %s \n", tableaux_graphique_carte[deck[i][j]],tableaux_graphique_couleur[deck_couleur[i][j]]);
+            if(deck[i][j] == deck[i][0]) // Condition de victoire pour les cartes
             {
                 compteur++;
                 if(compteur == 4+temp)
@@ -174,7 +174,7 @@ void affichage(int deck[13][4],int deck_couleur[13][4], int joueur,int joueur_ac
                     return 0;
                 }
             }
-            else if(deck_couleur[i][j] == deck_couleur[i][0]) // condition de victoire pour les couleurs
+            else if(deck_couleur[i][j] == deck_couleur[i][0]) // Condition de victoire pour les couleurs
             {
                 compteur2++;
                 if(compteur2 == 4+temp)
@@ -227,12 +227,12 @@ void echange(int deck[13][4],int deck_couleur[13][4],int joueur,int joueur_actu,
 {
     int i,j,valeur_joueur=0,carte=9,swap,votre_carte,votre_test,valeur_joueur_max=0;
 
-    affichage(deck,deck_couleur,joueur,joueur_actu,fake_win,nombre_de_carte); //affiche les cartes du joueur actuelle
+    affichage(deck,deck_couleur,joueur,joueur_actu,fake_win,nombre_de_carte); //Affiche les cartes du joueur actuel
 
-    valeur_joueur = 12-joueur_actu; //pour savoir sur quelle joueur nous somme actuellement
-    valeur_joueur_max = valeur_joueur-1; //pour le joueur suivant
+    valeur_joueur = 12-joueur_actu; //Pour savoir sur quel joueur nous sommes actuellement
+    valeur_joueur_max = valeur_joueur-1; //Pour le joueur suivant
 
-    if(valeur_joueur_max == 12-joueur) //pour mettre une limitation exemple il a 3 joueurs max je peux pas piocher dans un joueur 4
+    if(valeur_joueur_max == 12-joueur) //Pour mettre une limitation ; Exemple : il y a 3 joueurs max je peux pas échanger avec le joueur 4
     {
         valeur_joueur_max = 12;
     }
@@ -242,7 +242,7 @@ void echange(int deck[13][4],int deck_couleur[13][4],int joueur,int joueur_actu,
         {
             printf("Entrez votre carte : ");
             scanf("%d", &votre_carte);
-        }while(votre_carte < 0 || votre_carte > fake_win[i]); // fake_win fonctionne pour mettre une limitation dynamique pour chaque joueur
+        }while(votre_carte < 0 || votre_carte > fake_win[i]); // Fake_win fonctionne pour mettre une limitation dynamique pour chaque joueur
     }
     for(i=valeur_joueur_max;i>=valeur_joueur_max;i--)
     {
@@ -253,13 +253,13 @@ void echange(int deck[13][4],int deck_couleur[13][4],int joueur,int joueur_actu,
         }while(votre_test < 0 || votre_test > fake_win[i]);
     }
 
-    for(i=1;i<=1;i++) // fonction simple pour l'échange de carte
+    for(i=1;i<=1;i++) // Fonction simple pour l'échange de cartes
     {
         swap = deck[valeur_joueur][votre_carte];
         deck[valeur_joueur][votre_carte] = deck[valeur_joueur_max][votre_test];
         deck[valeur_joueur_max][votre_test] = swap;
     }
-    for(i=1;i<=1;i++) // fonction simple pour l'échange de couleur
+    for(i=1;i<=1;i++) // Fonction simple pour l'échange de couleurs
     {
         swap = deck_couleur[valeur_joueur][votre_carte];
         deck_couleur[valeur_joueur][votre_carte] = deck_couleur[valeur_joueur_max][votre_test];
@@ -267,7 +267,7 @@ void echange(int deck[13][4],int deck_couleur[13][4],int joueur,int joueur_actu,
     }
 }
 
-int verification(int deck[13][4],int joueur_actu,int joueur) // pour savoir qu'elle joueur a eu la dame en premier
+int verification(int deck[13][4],int joueur_actu,int joueur) // Pour savoir quel joueur a eu la dame en premier
 {
     int i,j,valeur_joueur;
 
@@ -290,14 +290,14 @@ void game(int deck[13][4],int deck_couleur[13][4],int joueur,int joueur_actu,int
     int i,j,choix,manche=0,temp,fin=0,compteur=0,joueur_actuelle=0,min=0,temp2;
     do
     {
-        if(manche == nombre_de_manche) // condition de fin de jeu, affiche le score de chaque joueur
+        if(manche == nombre_de_manche) // Condition de fin de jeu, affiche le score de chaque joueur
         {
             system("cls");
             for(i=0;i<joueur;i++)
             {
                 temp2 = kilo(deck,deck_couleur,joueur,i,fake_win);
                 printf("Score du joueur %d : %d \n",i,temp2);
-                if(temp2 > min) // pour savoir quel joueur a le plus haut score
+                if(temp2 > min) // Pour savoir quel joueur a le plus haut score
                 {
                     min = temp2;
                     temp = i;
@@ -329,7 +329,7 @@ void game(int deck[13][4],int deck_couleur[13][4],int joueur,int joueur_actu,int
             system("cls");
             echange(deck,deck_couleur,joueur,joueur_actu,fake_win,nombre_de_carte);
             joueur_actu++;
-            if(joueur_actu > joueur-1) // quand le joueur 3 a fini sont tour il passe au joueur 1 mais met fin la manche aussi
+            if(joueur_actu > joueur-1) // Quand le joueur 3 a fini son tour,  c'est à nouveau au joueur 1, mettant ainsi fin à une manche
             {
                 joueur_actu=0;
                 manche++;
@@ -337,7 +337,7 @@ void game(int deck[13][4],int deck_couleur[13][4],int joueur,int joueur_actu,int
             break;
         case 5:
             system("cls");
-            temp = kilo(deck,deck_couleur,joueur,joueur_actu,fake_win); // affiche le score du joueur x
+            temp = kilo(deck,deck_couleur,joueur,joueur_actu,fake_win); // Affiche le score du joueur x
             printf("Voici votre score : %d \n\n", temp);
             break;
         case 6:
@@ -353,7 +353,7 @@ void game(int deck[13][4],int deck_couleur[13][4],int joueur,int joueur_actu,int
             system("cls");
             compteur++;
             joueur_actuelle = 12-joueur_actu;
-            if(compteur==2) // compteur d'activation de la pioche d'une ieme carte pour un joueur x
+            if(compteur==2) // Compteur d'activation de la pioche d'une enième carte pour un joueur x
             {
                 nombre_de_carte--;
                 compteur=0;
@@ -405,7 +405,7 @@ int menu_game(int manche,int nombre_de_carte,int nombre_de_manche)
     return choix;
 }
 
-int kilo(int deck[13][4], int deck_couleur[13][4], int joueur, int joueur_actu,int fake_win[13]) // fonction pour donner le score
+int kilo(int deck[13][4], int deck_couleur[13][4], int joueur, int joueur_actu,int fake_win[13]) // Fonction pour donner le score
 {
     int i,j,joueur_actuelle,score=0;
 
